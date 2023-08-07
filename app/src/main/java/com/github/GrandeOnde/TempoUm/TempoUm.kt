2e73816ccf4749801be6abbbf0b34b3b4ae5e60b
Pedro <cedtecatividade@gmail.com>
2023-08-07 14:26:02 -0300

    Primeiro Envio

---
+++ app/src/main/java/com/github/GrandeOnde/TempoUm/TempoUm.kt
@@ -0,0 +1,11 @@
+package com.github.GrandeOnde.TempoUm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`TempoUm.xml`)
+    }
+}
