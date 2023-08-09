8989599a91252d7c02a6f5fbb5b0ccfaac861f9e
Pedro <cedtecatividade@gmail.com>
2023-08-09 15:21:58 -0300

    Calculando a gorjeta e definindo ela

--- app/src/main/java/com/github/GrandeOnde/TempoUm/TempoUm.kt
+++ app/src/main/java/com/github/GrandeOnde/TempoUm/TempoUm.kt
@@ -2,10 +2,34 @@ package com.github.GrandeOnde.TempoUm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.GrandeOnde.TempoUm.databinding.TempoUmBinding
+import java.lang.Math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: TempoUmBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`TempoUm.xml`)
+        binding = TempoUmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculaTudo() }
+    }
+
+    fun calculaTudo() {
+        val stringInTextField = binding.ComponenteEssencia.text.toString()
+        val custo = stringInTextField.toDouble()
+        val idSelecionado = binding.ComponenteSelect.checkedRadioButtonId
+        val gorjetaPorcent = when (idSelecionado) {
+            R.id.VinteCento -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var gorjeta = gorjetaPorcent * custo
+        val marcadoAcima = binding.Barra.isChecked
+        if (marcadoAcima) {
+            gorjeta = ceil(gorjeta)
+
+        }
     }
 }
